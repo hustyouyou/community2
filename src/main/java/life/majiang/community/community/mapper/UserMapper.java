dee3d458c36eb936ed220dfae555eac70bc6710d
hustyouyou <guangqianghust@163.com>
2019-12-07 15:35:37 +0800

    add check cookies

--- src/main/java/life/majiang/community/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.majiang.community.community.mapper;
 import life.majiang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by ygq on 2019/12/6 20:21
@@ -11,4 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
